6a5dbe05fb7de5aee064488d3b491020ba2b8d53
Hunter <hstohlmeyer@ou.edu>
2020-12-01 17:01:18 -0600

    Project 3

--- R/ShinyMLR.R
+++ R/ShinyMLR.R
@@ -10,11 +10,10 @@
 #' @export
 #'
 #' @examples
-#' data(package="s20x")
-#' data(camplake.df)
-#' data(books.df)
-#' ShinyMLR(data=camplake.df)
-#' ShinyMLR(data=books.df)
+#' quasar<-readxl::read_excel("C:/Users/hstoh/OneDrive - University of Oklahoma/Fall 2020/MATH 5773/Data/QUASAR.xls")
+#' bub<-readxl::read_excel("C:/Users/hstoh/OneDrive - University of Oklahoma/Fall 2020/MATH 5773/Data/BUBBLE2.xls")
+#' \dontrun{ ShinyMLR(data=quasar)}
+#' \dontrun{ ShinyMLR(data=bub)}
 #'
 ShinyMLR<-function(data){
 
@@ -22,45 +21,45 @@ ShinyMLR<-function(data){
 
   q<-names(data)
 
-  ui<-fluidPage(
-    titlePanel("MLR"),
-    sidebarPanel(
-      p("Select the input for the Dependent Variable"),
-      selectInput(inputId = "DepVar", label = "Dependent Variables", multiple = FALSE, choices = q),
-      p("Select the inputs for the Independent Variable"),
-      selectInput(inputId = "IndVar", label = "Independent Variables", multiple = TRUE, choices = q)
+  ui<-shiny::fluidPage(
+    shiny::titlePanel("MLR"),
+    shiny::sidebarPanel(
+      shiny::p("Select the input for the Dependent Variable"),
+      shiny::selectInput(inputId = "DepVar", label = "Dependent Variables", multiple = FALSE, choices = q),
+      shiny::p("Select the inputs for the Independent Variable"),
+      shiny::selectInput(inputId = "IndVar", label = "Independent Variables", multiple = TRUE, choices = q)
     ),
-    mainPanel(
-      verbatimTextOutput(outputId = "RegSum"),
-      verbatimTextOutput(outputId = "IndPrint"),
-      verbatimTextOutput(outputId = "DepPrint"),
-      verbatimTextOutput(outputId = "Normality"),
-      verbatimTextOutput(outputId = "MeanofError"),
-      verbatimTextOutput(outputId = "CIs")
+    shiny::mainPanel(
+      shiny::verbatimTextOutput(outputId = "RegSum"),
+      shiny::verbatimTextOutput(outputId = "IndPrint"),
+      shiny::verbatimTextOutput(outputId = "DepPrint"),
+      shiny::verbatimTextOutput(outputId = "Normality"),
+      shiny::verbatimTextOutput(outputId = "MeanofError"),
+      shiny::verbatimTextOutput(outputId = "CIs")
 
     )
   )
 
   server <- function(input, output) {
 
-    lm1 <- reactive({lm(reformulate(input$IndVar, input$DepVar), data = data)})
+    lm1 <- shiny::reactive({lm(reformulate(input$IndVar, input$DepVar), data = data)})
 
 
 
 
 
-    output$DepPrint <- renderPrint({input$DepVar})
-    output$IndPrint <- renderPrint({input$IndVar})
-    output$RegSum <- renderPrint({summary(lm1())})
-    output$Normality <- renderPrint({normcheck(lm1(),shapiro.wilk=TRUE)})
+    output$DepPrint <- shiny::renderPrint({input$DepVar})
+    output$IndPrint <- shiny::renderPrint({input$IndVar})
+    output$RegSum <- shiny::renderPrint({summary(lm1())})
+    output$Normality <- shiny::renderPrint({s20x::normcheck(lm1(),shapiro.wilk=TRUE)})
 
-    output$MeanofError <- renderPrint({
+    output$MeanofError <- shiny::renderPrint({
       model=lm(reformulate(input$IndVar, input$DepVar), data = data)
       res=residuals(model)
       mean(res)
     })
 
-    output$CIs <- renderPrint({
+    output$CIs <- shiny::renderPrint({
       model=lm(reformulate(input$IndVar, input$DepVar), data = data)
       s20x::ciReg(model)
 
